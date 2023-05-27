a47279aa92022f3d796e091100a8d2e095a0b7cb
Viktoria Gorozanina <gorozanina@gmail.com>
2023-05-27 21:26:45 +0300

    Scope examples

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you are a millenial`;
+      console.log(str);
+      //creating a new variable with the same name as in the outer scope's variable. It will be used, since variable look=up will stop after searching in this scope.
+      const firstName = `Nick`;
+      //reassigning outer scope's variable
+      output = `New output!`;
+
+      function add(a, b) {
+        return a + b;
+      }
+      //   console.log(add(2, 3));
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = `Victoria`;
+calcAge(1991);
