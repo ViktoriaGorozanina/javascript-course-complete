bc1805f137e13716f4fc9d5f698d305f0dbc4699
Viktoria Gorozanina <gorozanina@gmail.com>
2023-05-30 02:57:50 +0300

    Lesson 94, 95

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,29 +1,68 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
-  function printAge() {
-    const output = `${firstName} you are ${age}, born in ${birthYear}`;
-    console.log(output);
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   function printAge() {
+//     const output = `${firstName} you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      const str = `Oh, and you are a millenial`;
-      console.log(str);
-      //creating a new variable with the same name as in the outer scope's variable. It will be used, since variable look=up will stop after searching in this scope.
-      const firstName = `Nick`;
-      //reassigning outer scope's variable
-      output = `New output!`;
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = `Oh, and you are a millenial`;
+//       console.log(str);
+//       //creating a new variable with the same name as in the outer scope's variable. It will be used, since variable look=up will stop after searching in this scope.
+//       const firstName = `Nick`;
+//       //reassigning outer scope's variable
+//       output = `New output!`;
 
-      function add(a, b) {
-        return a + b;
-      }
-      //   console.log(add(2, 3));
-    }
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       //   console.log(add(2, 3));
+//     }
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = `Victoria`;
-calcAge(1991);
+// const firstName = `Victoria`;
+// calcAge(1991);
+
+//-------------------LESSON 95
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+// var me = `Vic`;
+// let job = `Dev`;
+// const year = 1986;
+
+//FUnctions
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+//Example
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log(`all Products deleted`);
+// }
+
+//Other example
+
+var x = 1;
+let y = 2;
+const z = 3;
