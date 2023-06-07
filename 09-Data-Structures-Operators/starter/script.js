1b6e6b7c589193e74036823283fd6115deaf5803
Viktoria Gorozanina <gorozanina@gmail.com>
2023-06-08 02:22:17 +0300

    finished 103

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,30 @@ const restaurant = {
     },
   },
 };
+//Receieve 2 return values from a function:
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+//Nested destructuring:
+const nested = [1, 2, [5, 6]];
+const [, , [ha, ho]] = nested;
+console.log(ha, ho);
+// Default valeus (if we try to reach an element that is not existing in the array the we can set a default value):
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+//
+const arr = [1, 2, 3];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+//or
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
+// to skip elements we use space
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+// to swap it we reassign it:
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
