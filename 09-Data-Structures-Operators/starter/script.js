0f4a0fc5ef601658d63c1d00c904505adcf2ad09
Viktoria Gorozanina <gorozanina@gmail.com>
2023-07-08 01:09:54 +0300

    Lesson 118 finished

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -667,37 +667,43 @@ const restaurant = {
 
 //Data structure that we can use to map values to keys
 
-const rest = new Map();
-rest.set(`name`, `Classico Italiano`);
-rest.set(`name`, `NewName`);
-rest.set(1, `Firenze, Italy`);
-rest.set(2, `Lisbon, Portugal`);
-
-rest
-  .set(`categories`, ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set(`open`, 11)
-  .set(`close`, 23)
-  .set(true, `We are open`)
-  .set(false, `We are closed`);
-
-console.log(rest.get(`name`));
-
-//usecase
-const time = 21;
-console.log(rest.get(time > rest.get(`open`) && time < rest.get(`close`)));
 //
-console.log(rest.has(`categories`)); //true. boolean result
-console.log(rest.delete(2));
-console.log(rest.get(1));
+//*------------Lesson 118
+//another way to populate a new map:
 
-rest.set([1, 2], `Test`);
-console.log(rest.get([1, 2])); //undefined
-// to use an array we create a variable for it:
-const arr = [1, 2];
-rest.set(arr, `Test`);
-rest.set(document.querySelector(`h1`), `Heading`);
-console.log(rest.get(arr));
+// const question = new Map([
+//   [`question`, `What is the best programming language?`],
+//   [1, `C`],
+//   [2, `Java`],
+//   [3, `Javascript`],
+//   [`correct`, 3],
+//   [true, `correct!!`],
+//   [false, `Try again`],
+// ]);
 
-console.log(rest);
-console.log(rest.size);
-// console.log(rest.clear);
+// console.log(question);
+// //COnvert object to map:
+// console.log(Object.entries(openingHours));
+
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// //FOr loop:
+// //Quizz
+// console.log(question.get(`question`));
+// for (const [key, value] of question) {
+//   if (typeof key === `number`) console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = Number(prompt(`Your answer:`));
+// console.log(answer);
+
+// console.log(question.get(answer === question.get(`correct`)));
+
+// //Convert map to an array:
+// console.log(...question);
+// console.log(...question.entries());
+// console.log(...question.keys());
+// console.log(...question.values());
+
+//*------------Lesson 119
+//
