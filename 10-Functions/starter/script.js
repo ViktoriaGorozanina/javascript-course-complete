413166717789367346dbf160061681c91767b462
Viktoria Gorozanina <gorozanina@gmail.com>
2023-07-16 03:05:46 +0300

    Lesson 128 finished

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const bookings = [];
+
+const createBooking = function (flightNum,numPassengers = 1, price=199) {
+
+
+    // numPassengers = numPassengers || 1; //default value 1. ES5 way of doing it
+    // price = price || 199;//instead we just type it straight into the function values
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+
+    console.log(booking);
+    bookings.push(booking);
+};
+
+createBooking(`LH123`);
+createBooking(`LH123`,2, 200);
+createBooking(`LH123`,undefined, 200);
