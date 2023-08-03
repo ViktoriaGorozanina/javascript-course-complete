469b24d1a5b9cd8e9c28b3ae2b9ccb485999b7b4
Viktoria Gorozanina <gorozanina@gmail.com>
2023-08-03 23:48:29 +0300

    Lessons 149-150 finished

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -2,112 +2,6 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
-
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-
-
-//*----------------------Lesson 147
-const displayMovements = function(movements) {
-
-containerMovements.innerHTML = ``;//innerHTML is similar to textContent, but textContext return text, while innerHTML returns everyting in HMTL
-
-movements.forEach(function(mov, i) {
-const type = mov > 0 ? `deposit` : `withdrawal`
-
-  //create HTML element
-  const html = `
-  <div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
-    <div class="movements__value">${mov}</div>
-  </div>`;
-  containerMovements.insertAdjacentHTML(`afterbegin`, html)
-})
-}
-displayMovements(account1.movements);
-
-// console.log(containerMovements.innerHTML);
-
-//?----------------------Lesson 148 CHALLENGE - PASSED
-
-const dogsJulia = [9, 16, 6, 8, 3];
-const dogsKate = [10, 5, 6, 1, 4];
-
-
-const checkDogs = function(dogs) {
-return dogs.slice(1,3)
-}
-
-const dogsJuliaKate = dogsKate.concat(checkDogs(dogsJulia))
-console.log(dogsJuliaKate);
-
-dogsJuliaKate.forEach(function(age, i) {
-if(age>=3) {
-  console.log(`Dog number ${i+1} is an adult, and is ${age} years old`);
-} else {
-  console.log(`Dog number ${i+1} is still a puppy, and is ${age} years old`);
-}
-})
-
-console.log(dogsJulia);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // LECTURES
 
 // const currencies = new Map([
@@ -116,7 +10,7 @@ console.log(dogsJulia);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 //*----------------------Lesson 142
@@ -213,12 +107,143 @@ console.log(dogsJulia);
 //   console.log(`value: ${value}. key: ${key}, set: ${set}`);
 // })
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+
+//*----------------------Lesson 147
+const displayMovements = function(movements) {
+
+containerMovements.innerHTML = ``;//innerHTML is similar to textContent, but textContext return text, while innerHTML returns everyting in HMTL
+
+movements.forEach(function(mov, i) {
+const type = mov > 0 ? `deposit` : `withdrawal`
+
+  //create HTML element
+  const html = `
+  <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+  containerMovements.insertAdjacentHTML(`afterbegin`, html)
+})
+}
+displayMovements(account1.movements);
+
+// console.log(containerMovements.innerHTML);
+
+//?----------------------Lesson 148 CHALLENGE - PASSED
+
+// const dogsJulia = [9, 16, 6, 8, 3];
+// const dogsKate = [10, 5, 6, 1, 4];
+
+
+// const checkDogs = function(dogs) {
+// return dogs.slice(1,3)
+// }
+
+// const dogsJuliaKate = dogsKate.concat(checkDogs(dogsJulia))
+// console.log(dogsJuliaKate);
+
+// dogsJuliaKate.forEach(function(age, i) {
+// if(age>=3) {
+//   console.log(`Dog number ${i+1} is an adult, and is ${age} years old`);
+// } else {
+//   console.log(`Dog number ${i+1} is still a puppy, and is ${age} years old`);
+// }
+// })
+
+// console.log(dogsJulia);
+
+//*----------------------Lesson 149
+
+//MAP, FILTER, REDUCE
+
+//*----------------------Lesson 149
+
+//MAP
+
+const eurToUsd = 1.1;
+const movementsUsd = movements.map(function(mov){
+  return mov*eurToUsd;
+})
+//same with arrow function:
+const movementsUsd2 = movements.map(mov => mov * eurToUsd);
+console.log(movementsUsd2);
 
 
 
+console.log(movements);
+console.log(movementsUsd);
 
+const movementsUsdFor = []
+for(const mov of movements)movementsUsdFor.push(mov * eurToUsd);
+console.log(movementsUsdFor);
 
+const movementsDescription = movements.map((mov, i, arr) =>  `Movement ${i+1}: You ${mov > 0 ? `deposited` : `withdrew`}`
+  // {if (mov>0) {
+  //   return `Movement ${i+1}: You deposited ${mov}`
+  // } else {
+  //   return `Movement ${i+1}: You withdrew ${Math.abs(mov)}`
+  // }}
+)
 
-
-
-
+console.log(movementsDescription);
