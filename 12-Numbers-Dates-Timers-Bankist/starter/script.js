0a45d5a14582cf645c65c6f13bfecb59e4577ce7
Viktoria Gorozanina <gorozanina@gmail.com>
2023-08-17 21:10:20 +0300

    Up to lesson 170 finished

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,57 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+//*----------------------Lesson 170
+
+// console.log(23 === 23.0);// true
+
+// //Base 10 : 0 to 9
+
+// //Base binary 2: 0 and 1
+// console.log(`1`);
+// console.log(0.1 + 0.2);// 0.30000000000000004
+
+// console.log(0.1 + 0.2 === 0.3);// false !
+// const trunc = Math.trunc(0.1 + 0.2)
+// console.log(trunc === 0.3);// even that is false !
+// console.log(`1`);
+
+// //another way to transform a string to a number:
+// console.log(Number(`23`));
+// console.log(+ `23`); //once JS sees + it takes it as calculations and convert string to number automatically
+
+// //PARSING
+// console.log(Number.parseInt(`30.5px`, 10));//30 / the number must be in the beginning
+// console.log(Number.parseInt(`e30px`, 10));//NaN since first is a letter
+
+// console.log(Number.parseFloat(`2.5rem`));//2.5  / the number must be in the beginning
+// console.log(parseFloat(`2.5rem`));//2.5
+// console.log(parseFloat(`e2.5rem`));//NaN since first is a letter
+// console.log(`here`);
+
+// //NaN, check if value is not a number
+// console.log(Number.isNaN(20));//false
+// console.log(Number.isNaN(`20`));//false
+// console.log(Number.isNaN(+`20`));//false
+// console.log(Number.isNaN(+`20fx`));//true
+// console.log(Number.isNaN(23 / 0));//false since infinity
+
+// //isFinite, chceking if value is number
+// console.log(Number.isFinite(20));//true
+// console.log(Number.isFinite(`20`));//false
+// console.log(Number.isFinite(+`20`));//true
+// console.log(Number.isFinite(+`20c`));//false
+// console.log(Number.isFinite(23 / 0));//false
+
+
+//*----------------------Lesson 171
+//Square ROOT
+console.log(Math.sqrt(25));//5
+//the same result:
+console.log(25 **(1/2));//5
+console.log(8 ** (1/3));//cubic root
+
+console.log(Math.max(5,18,23,11,2));//23 returns the biggest value, it does not parse
+
