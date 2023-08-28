f022c09ecaedc6ed7540b6e03c5d0916e4b9bf54
Viktoria Gorozanina <gorozanina@gmail.com>
2023-08-28 22:10:24 +0300

    Lesson 187 finished

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,43 +33,97 @@ document.addEventListener('keydown', function (e) {
 //*-------------------Lesson 186
 
 //SELECTING
-// console.log(document.documentElement);//selects the entire HTML
-// console.log(document.head);//selects head of HTML
-// console.log(document.body);//selects body of HTML
+console.log(document.documentElement);//selects the entire HTML
+console.log(document.head);//selects head of HTML
+console.log(document.body);//selects body of HTML
 
-// const header = document.querySelector(`.header`);//selects class
-// const allSections = document.querySelectorAll(`.section`);//selects all
-// console.log(allSections);//returns Node list
+const header = document.querySelector(`.header`);//selects class
+const allSections = document.querySelectorAll(`.section`);//selects all
+console.log(allSections);//returns Node list
 
-// document.getElementById(`section--1`);//select by id name
+document.getElementById(`section--1`);//select by id name
 
-// const allButtons = document.getElementsByTagName(`button`);//selects all elements with selected tags 
+const allButtons = document.getElementsByTagName(`button`);//selects all elements with selected tags 
 
-// console.log(allButtons);//returns HTML collection
+console.log(allButtons);//returns HTML collection
 
-// const btn = document.getElementsByClassName(`btn`);
-// console.log(btn);//gets HTML collection of all btn class elements
+const btn = document.getElementsByClassName(`btn`);
+console.log(btn);//gets HTML collection of all btn class elements
 
 
-// //CREATING and INSERTING html elements
-// // .insertAdjacentHTML
+//CREATING and INSERTING html elements
+// .insertAdjacentHTML
 
-// const message = document.createElement(`div`);
-// message.classList.add(`cookie-message`);//adds class name
-// message.textContent = `We use cookies for analytics.`;
-// message.innerHTML = `We use cookies for analytics. <button class="btn btn--close-cookie">Got it"!</button>`;
+const message = document.createElement(`div`);
+message.classList.add(`cookie-message`);//adds class name
+message.textContent = `We use cookies for analytics.`;
+message.innerHTML = `We use cookies for analytics. <button class="btn btn--close-cookie">Got it"!</button>`;
 
-// //we need to insert the created element into our DOM:
-// // header.prepend(message);
-// header.append(message)
-// // header.append(message.cloneNode(true));//now the element will be in two places since we cloned two out of 1
+//we need to insert the created element into our DOM:
+// header.prepend(message);
+header.append(message)
+// header.append(message.cloneNode(true));//now the element will be in two places since we cloned two out of 1
 
-// // header.before(message);//creates an el. before the selected element (header) as a sibling
-// // header.after(message.cloneNode);//creates an el. after the selected element (header) as a sibling
+// header.before(message);//creates an el. before the selected element (header) as a sibling
+// header.after(message.cloneNode);//creates an el. after the selected element (header) as a sibling
 
-// //DELETE elements
-// document.querySelector(`.btn--close-cookie`).addEventListener(`click`, function(){
-//   message.remove();
-// })
+//DELETE elements
+document.querySelector(`.btn--close-cookie`).addEventListener(`click`, function(){
+  message.remove();
+})
 
-//*-------------------Lesson 187
+//*-------------------Lesson 187 
+
+//STYLES
+message.style.backgroundColor = `#37383d`;
+message.style.width = `120%`;
+// message.style.padding = `10px 0px 10px 0px`;
+//we can't read styles from css:
+console.log(message.style.height);
+console.log(message.style.backgroundColor);//reads only the color that we set above here in JS
+
+//GET STYLE FROM CSS
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+//increase height
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + `px`;
+console.log(getComputedStyle(message).height);
+
+//CSS custom properties (variables)
+document.documentElement.style.setProperty(`--color-primary`, `orangered`)
+
+//ATTRIBUTES
+const logo = document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);//class (before it was `className` in html)
+//shows only standard properties
+
+logo.alt = `Hello you`
+console.log(logo.alt);
+//Non-standard
+// console.log(logo.designer);//wont work since its non standard
+console.log(logo.getAttribute(`designer`));
+logo.setAttribute(`company`, `Vicky`)
+console.log(logo.company);
+
+//to get real src from HTML
+console.log(logo.src);//absolute link - http://127.0.0.1:5500/13-Advanced-DOM-Bankist/starter/img/logo.png
+console.log(logo.getAttribute(`src`));//img/logo.png
+
+const link = document.querySelector(`.twitter-link`);
+console.log(link.href);
+console.log(link.getAttribute(`href`));
+
+//DATA attributes
+console.log(logo.dataset.versionNumber);
+
+
+//CLASSES
+logo.classList.add(`class`);//can pass multiple classes
+logo.classList.remove(`class`);//can pass multiple classes
+logo.classList.toggle(`class`);//can pass multiple classes
+logo.classList.contains(`class`);//not includes. can pass multiple classes
+
+//set a class
+logo.className = `Jonas`//dont use, it will overwrite  all teh existing classes and allows to put only 1 class
