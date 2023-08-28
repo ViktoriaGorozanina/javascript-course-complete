a4542731669e1b67d8a37388f71769d3220cf00a
Viktoria Gorozanina <gorozanina@gmail.com>
2023-08-28 03:35:24 +0300

    Lessons up to 187 finished

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//*-------------------Lesson 186
+
+//SELECTING
+// console.log(document.documentElement);//selects the entire HTML
+// console.log(document.head);//selects head of HTML
+// console.log(document.body);//selects body of HTML
+
+// const header = document.querySelector(`.header`);//selects class
+// const allSections = document.querySelectorAll(`.section`);//selects all
+// console.log(allSections);//returns Node list
+
+// document.getElementById(`section--1`);//select by id name
+
+// const allButtons = document.getElementsByTagName(`button`);//selects all elements with selected tags 
+
+// console.log(allButtons);//returns HTML collection
+
+// const btn = document.getElementsByClassName(`btn`);
+// console.log(btn);//gets HTML collection of all btn class elements
+
+
+// //CREATING and INSERTING html elements
+// // .insertAdjacentHTML
+
+// const message = document.createElement(`div`);
+// message.classList.add(`cookie-message`);//adds class name
+// message.textContent = `We use cookies for analytics.`;
+// message.innerHTML = `We use cookies for analytics. <button class="btn btn--close-cookie">Got it"!</button>`;
+
+// //we need to insert the created element into our DOM:
+// // header.prepend(message);
+// header.append(message)
+// // header.append(message.cloneNode(true));//now the element will be in two places since we cloned two out of 1
+
+// // header.before(message);//creates an el. before the selected element (header) as a sibling
+// // header.after(message.cloneNode);//creates an el. after the selected element (header) as a sibling
+
+// //DELETE elements
+// document.querySelector(`.btn--close-cookie`).addEventListener(`click`, function(){
+//   message.remove();
+// })
+
+//*-------------------Lesson 187
