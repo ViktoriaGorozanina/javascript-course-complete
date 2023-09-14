68fcfc26a5c72c5020e3a5197f272c52d63d82c6
Viktoria Gorozanina <gorozanina@gmail.com>
2023-09-15 01:38:06 +0300

    Lessons up to 214 finished

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -85,11 +85,11 @@ const Car = function (make, speed) {
 
 Car.prototype.accelerate = function () {
   this.speed = this.speed + 10;
-  console.log(this.speed);
+  // console.log(this.speed);
 };
 Car.prototype.brake = function () {
   this.speed = this.speed - 5;
-  console.log(this.speed);
+  // console.log(this.speed);
 };
 
 const bmw = new Car(`BMW`, 120);
@@ -99,3 +99,88 @@ bmw.accelerate();
 bmw.brake();
 mercedes.accelerate();
 mercedes.brake();
+
+//*---------------- LESSON 213
+//CLASSES
+
+//class expression:
+// const Person = class {}
+//OR
+//Class declaration:
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    // console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl(`Jessica`, 1980);
+
+// console.log(jessica);
+
+// console.log(jessica.__proto__);
+// console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  // console.log(`Hey, ${this.firstName}`);
+};
+jessica.greet();
+
+//1. classes are NOT hoisted (means we can NOT use them BEFORE they are declared)
+//2. Classes are 1st class cityzens (we can pass them to functions and to return them from functions)
+//3. Classes are executed in `strict mode`
+
+//*---------------- LESSON 214
+//GETTERS and SETTERS
+
+const account = {
+  owner: `Jonas`,
+  movements: [200, 530, 120, 300],
+
+  // to add a getter to this object :
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  //set - will add a new mov into the movements, must have an argument
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest); //we write it just like a usual object property
+
+account.latest = 50;
+console.log(account.movements);
+
+//setter is great for data validation:
+
+class PersonCl2 {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    // console.log(2037 - this.birthYear);
+  }
+  //OR
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  //set a property that already exists:
+  set fullName(name) {
+    // console.log(name);
+    if (name.includes(` `)) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+const jess = new PersonCl2(`jess pess`, 1980);
+console.log(jess);
