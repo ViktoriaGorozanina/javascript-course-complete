26af3215ace77d2a26cf3165b0c16d19993e7403
Viktoria Gorozanina <gorozanina@gmail.com>
2023-09-14 23:19:29 +0300

    Challenge #1 - PASSED

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -56,21 +56,46 @@ Person.prototype.species = `Homosapiens`;
 
 //*---------------- LESSON 211
 
-console.log(Victoria.__proto__);
-console.log(Victoria.__proto__.__proto__);
+// console.log(Victoria.__proto__);
+// console.log(Victoria.__proto__.__proto__);
 
-console.log(Person.prototype.constructor);
+// console.log(Person.prototype.constructor);
 
 const arr = [1, 2, 5, 2, 5, 2];
-console.log(arr.__proto__); //each array inherits all methods from the default array default construction
-console.log(arr.__proto__ === Array.prototype); //true
-console.log(arr.__proto__.__proto__); //leads to the object.prototype
+// console.log(arr.__proto__); //each array inherits all methods from the default array default construction
+// console.log(arr.__proto__ === Array.prototype); //true
+// console.log(arr.__proto__.__proto__); //leads to the object.prototype
 
 //we add a new method to an array construction and we can call this method anytime (shouldnt use this, may be on a small projects, for maintanence reasons and if JS updates with the same method name but different action)
 Array.prototype.unique = function () {
   return [...new Set(this)];
 };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
 const h1 = document.querySelector(`h1`);
+// console.dir(x => x + 1);
+
+//?---------------- LESSON 212 CHALLENGE#1 - PASSED
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+const bmw = new Car(`BMW`, 120);
+const mercedes = new Car(`Mercedes`, 95);
+
+bmw.accelerate();
+bmw.brake();
+mercedes.accelerate();
+mercedes.brake();
