4d2fb7f181d61965f1f6c3e3c4693b6a53e87c90
Viktoria Gorozanina <gorozanina@gmail.com>
2023-09-29 00:43:44 +0300

    Lesson 234 finished

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -26,16 +26,31 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
+      console.log(map);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on(`click`, function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: `running-popup`,
+            })
+          )
+
+          .setPopupContent('<p>Running<br />workout</p>')
+          .openPopup();
+      });
     },
     //end lesson 233
 
@@ -46,3 +61,7 @@ if (navigator.geolocation) {
 }
 
 //* ------------------------- LESSON 233
+//added code in the previous lesson
+
+//* ------------------------- LESSON 234
+//added code in the previous lesson
