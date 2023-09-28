c86392cc961d5401de4bb23fb563d31e301b61e8
Viktoria Gorozanina <gorozanina@gmail.com>
2023-09-28 17:40:46 +0300

    Lesson 232 finished

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//* ------------------------- LESSON 232
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude); //59.4477056 24.8741888
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert(`Couldn't get your position`);
+    }
+  );
+}
+
+//* ------------------------- LESSON 233
