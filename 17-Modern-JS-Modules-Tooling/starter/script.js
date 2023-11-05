913573d17f7da9a73d9e7608e4695204de09e542
Viktoria Gorozanina <gorozanina@gmail.com>
2023-11-06 01:20:36 +0200

    274 - Module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -19,19 +19,44 @@ import anyName, { cart } from './shoppingCart.js';
 // const data = await res.json();
 // console.log(data);
 
-const getLastPost = async function () {
-  const res = await fetch(`https://jsonplaceholder.org/users`);
-  const data = await res.json();
-  console.log(data);
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.org/users`);
+//   const data = await res.json();
+//   console.log(data);
 
-  return {
-    firstName: data.at(-1).firstname,
-    lastName: data.at(-1).lastname,
-  };
-};
+//   return {
+//     firstName: data.at(-1).firstname,
+//     lastName: data.at(-1).lastname,
+//   };
+// };
 // const lastPost = getLastPost();
 // console.log(lastPost); //it will return a promise so we have to call .then method on it:
 // lastPost.then(res => console.log(res));//but instead we can use top-level await
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 275;
+  const totalQuantity = 55;
+  const addToCart = function (item, quantity) {
+    cart.push({ item, quantity });
+    console.log(`${quantity} ${item} added to cart`);
+  };
+  const ordredStock = function (item, quantity) {
+    cart.push({ item, quantity });
+    console.log(`${quantity} ${item} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+ShoppingCart2.addToCart(`apple`, 4);
+ShoppingCart2.addToCart(`juice`, 2);
+console.log(ShoppingCart2);
